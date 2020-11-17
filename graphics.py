04256c337f973e4ed4967e3967ee802c31bd32fa
agforero <agforero@users.noreply.github.com>
2020-11-16 22:56:24 -0600

    added animation with file intake

--- graphics.py
+++ graphics.py
@@ -1,21 +1,94 @@
-import sys, random, tkinter as tk
+import random, time, tkinter as tk
 
 def generateEvent(spacing, variability):
     return random.randint(spacing-variability, spacing+variability)
 
+# generally, this class operates under the assumtion that tiles are something like
+# [floor, sky].
+# so something like [0, 1] would mean there's NO tile on the floor, but IS one in the sky.
 class Level:
-    def __init__(self, width=100, spacing=6, variability=1):
+    def __init__(self, width=100, spacing=6, variability=1, readIn=""):
+        # this implies that data.txt is structured as follows:
+        # one line is the map, with pairs of numbers as each tile
+        # for example, 00 00 01 00 10 is then
+        # 0 0 1 0 0
+        # 0 0 0 0 1
+        # the next line is movements taken by the winner
+
         self.level = []
-        event = generateEvent(spacing, variability)
-        for i in range(width):
-            if event > 0:
-                self.level.append([0, 0])
-                event -= 1
+        self.movements = []
+
+        if readIn != "":
+            f = open(readIn, 'r')
+            data = []
+            for line in f:
+                data.append(line.split())
+
+            for tile in data[0]:
+                self.level.append([int(tile[0]), int(tile[1])])
+
+            for move in data[1][0]:
+                self.movements.append(int(move))
+
+            print(data)
+
+        else: 
+            event = generateEvent(spacing, variability)
+            for i in range(width):
+                if event > 0:
+                    self.level.append([0, 0])
+                    event -= 1
+                else:
+                    isInSky = random.randint(0, 1) % 2 == 0
+                    if isInSky: self.level.append([0, 1])
+                    else: self.level.append([1, 0])
+                    event = generateEvent(spacing, variability)
+
+    def drawLevel(self, w):
+        for i in range(len(self.level)):
+            bottomColor = "white"
+            skyColor = "white"
+
+            if self.level[i][0] == 1: bottomColor = "black"
+            if self.level[i][1] == 1: skyColor = "black"
+
+            x1 = SQUARESIZE * i
+            y1 = 0
+            x2 = x1 + SQUARESIZE
+            y2 = y1 + SQUARESIZE
+            x3 = x1
+            y3 = SQUARESIZE
+            x4 = x3 + SQUARESIZE
+            y4 = y3 + SQUARESIZE
+
+            # drawing floor square
+            w.create_rectangle(x3, y3, x4, y4, fill=bottomColor)
+
+            # drawing sky square
+            w.create_rectangle(x1, y1, x2, y2, fill=skyColor)
+
+    def animate(self, top, w, i=0):
+        if i >= len(self.level): return
+        else:
+            x1 = SQUARESIZE * i
+            y1 = 0
+            x2 = x1 + SQUARESIZE
+            y2 = y1 + SQUARESIZE
+            x3 = x1
+            y3 = SQUARESIZE
+            x4 = x3 + SQUARESIZE
+            y4 = y3 + SQUARESIZE
+
+            # hero stays on ground
+            if self.movements[i] == 0:
+                w.create_rectangle(x3, y3, x4, y4, fill="green")
             else:
-                isInSky = random.randint(0, 1) % 2 == 0
-                if isInSky: self.level.append([0, 1])
-                else: self.level.append([1, 0])
-                event = generateEvent(spacing, variability)
+                w.create_rectangle(x1, y1, x2, y2, fill="green")
+
+            top.after(100, lambda: self.animate(top, w, i+1))
+
+    def getLevelWidth(self):
+        return len(self.level)
 
     def __str__(self):
         top = ""
@@ -26,39 +99,21 @@ class Level:
         return f"{top}\n{bottom}"
 
 def main():
-    LEVELWIDTH = 100
-    SQUARESIZE = 15
-    l = Level(width=LEVELWIDTH, variability=3)
+    global SQUARESIZE # squares will be SQUARESIZE by SQUARESIZE pixels in area
+    SQUARESIZE = 15 # yes we need two lines here
+
+    l = Level(variability=3, readIn="data.txt")
+    print(l)
     
     top = tk.Tk()
-    w = tk.Canvas(top, width=LEVELWIDTH*SQUARESIZE, height=SQUARESIZE*2)
+    windowWidth = max((l.getLevelWidth()*SQUARESIZE), SQUARESIZE*15) # we want the window to be at least 15 tiles in width for good display purposes
+    w = tk.Canvas(top, width=windowWidth, height=SQUARESIZE*2)
     w.pack()
 
-    for i in range(len(l.level)):
-        bottomColor = "white"
-        skyColor = "white"
+    l.drawLevel(w)
 
-        if l.level[i][0] == 1: bottomColor = "black"
-        if l.level[i][1] == 1: skyColor = "black"
-
-        x1 = SQUARESIZE * i
-        y1 = 0
-
-        x2 = x1 + SQUARESIZE
-        y2 = y1 + SQUARESIZE
-
-        x3 = x1
-        y3 = SQUARESIZE
-
-        x4 = x3 + SQUARESIZE
-        y4 = y3 + SQUARESIZE
-
-        # drawing floor square
-        w.create_rectangle(x1, y1, x2, y2, fill=bottomColor)
-
-        # drawing sky square
-        w.create_rectangle(x3, y3, x4, y4, fill=skyColor)
-
+    animateButton = tk.Button(top, text="Animate!", command=lambda: l.animate(top, w))
+    animateButton.pack()
     tk.mainloop()
 
 if __name__ == "__main__":
