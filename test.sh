da5fa333d2e18d79c6344f236598ded59636d5f2
Rob Berger <robertberger5@gmail.com>
2020-11-21 15:43:33 -0600

    newer test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,22 @@ echo $OUTPUT_FILE
 
 touch $OUTPUT_FILE
 
-for a in {1..5} # test deleteMoves
+mc 1 5
+ps 1 100 by 5
+
+for d in {3..9} # test deleteMoves
 do
-	python3 GeoGame.py -na -ng -deleteMoves $a >> $OUTPUT_FILE
+	for pk in {1..33..3} # test percentKeep
+	do
+		for mc in {1..5} # test mutationChance
+		do
+			for ps in {1..100..5} # test populationSize
+			do
+				#test each 10 times
+
+				#also print out vars
+				python3 GeoGame.py -g -d $a -pk $(bc <<< $pk/100) -mc $(bc <<< $mc/100) -ps $ps >> $OUTPUT_FILE
+			done
+		done
+	done
 done
